d52dfc0b868020440adcacb0d94e8c2a5dfb63c7
Alexander Plotnikov <Appplotnikov2019@gmail.com>
2023-05-03 19:09:39 +0300

    New version

--- Homework Наследование N 3/main.cpp
+++ Homework Наследование N 3/main.cpp
@@ -32,8 +32,8 @@ protected:
 	int amount() override {
 		return 3;
 	}
-	Triangle(int a, int b, int c, int A, int B, int C, std::string Name) : a(a), b(b), c(c), A(A), B(B), C(C), Figure(Name) {}
 public:
+	Triangle(int a, int b, int c, int A, int B, int C, std::string Name) : a(a), b(b), c(c), A(A), B(B), C(C), Figure(Name) {}
 	Triangle() : Triangle(10, 20, 30, 50, 60, 70, "Triangle") {}
 	void print_info() override {
 		std::cout << Name << ":" << std::endl;
@@ -61,8 +61,8 @@ protected:
 	int amount() override {
 		return 4;
 	}
-	Quadrangle(int a1, int b1, int c1, int d1, int A1, int B1, int C1, int D1, std::string Name) : a1(a1), b1(b1), c1(c1), d1(d1), A1(A1), B1(B1), C1(C1), D1(D1), Figure(Name) {}
 public:
+	Quadrangle(int a1, int b1, int c1, int d1, int A1, int B1, int C1, int D1, std::string Name) : a1(a1), b1(b1), c1(c1), d1(d1), A1(A1), B1(B1), C1(C1), D1(D1), Figure(Name) {}
 	Quadrangle() : Quadrangle(10, 20, 30, 40, 50, 60, 70, 80, "Quadrangle") {}
 	void print_info() override {
 		std::cout << Name << ":" << std::endl;
@@ -75,85 +75,57 @@ public:
 };
 
 class RectangularTriangle : public Triangle {
+protected:
+	std::string check() override {
+		return "Wrong";
+	}
 public:
 	RectangularTriangle() : Triangle(10, 20, 30, 50, 60, 90, "Rectangular triangle") {}
-	void print_info() override {
-		std::cout << Name << ":" << std::endl;
-		std::cout << check() << std::endl;
-		std::cout << "Amount of sides: " << amount() << std::endl;
-		std::cout << "Sides: a = " << a << ", b = " << b << ", c = " << c << std::endl;
-		std::cout << "Angles: A = " << A << ", B = " << B << ", C = " << C << std::endl;
-		std::cout << std::endl;
-	}
 };
 
 class IsoscelesTriangle : public Triangle {
+protected:
+	std::string check() override {
+		return "Wrong";
+	}
 public:
 	IsoscelesTriangle() : Triangle(10, 20, 10, 50, 60, 50, "Isosceles triangle") {}
-	void print_info() override {
-		std::cout << Name << ":" << std::endl;
-		std::cout << check() << std::endl;
-		std::cout << "Amount of sides: " << amount() << std::endl;
-		std::cout << "Sides: a = " << a << ", b = " << b << ", c = " << c << std::endl;
-		std::cout << "Angles: A = " << A << ", B = " << B << ", C = " << C << std::endl;
-		std::cout << std::endl;
-	}
 };
 
 class EqualateralTriangle : public Triangle {
-	protected:
-		std::string check() override {
-			return "Right";
-		}
+protected:
+	std::string check() override {
+		return "Right";
+	}
 public:
 	EqualateralTriangle() : Triangle(30, 30, 30, 60, 60, 60, "Equalateral triangle") {}
-	void print_info() override {
-		std::cout << Name << ":" << std::endl;
-		std::cout << check() << std::endl;
-		std::cout << "Amount of sides: " << amount() << std::endl;
-		std::cout << "Sides: a = " << a << ", b = " << b << ", c = " << c << std::endl;
-		std::cout << "Angles: A = " << A << ", B = " << B << ", C = " << C << std::endl;
-		std::cout << std::endl;
-	}
 };
 
 class Parallelogram : public Quadrangle {
+protected:
+	std::string check() override {
+		return "Wrong";
+	}
 public:
 	Parallelogram() : Quadrangle(20, 30, 20, 30, 30, 40, 30, 40, "Parallelogram") {}
-	void print_info() override {
-		std::cout << Name << ":" << std::endl;
-		std::cout << check() << std::endl;
-		std::cout << "Amount of sides: " << amount() << std::endl;
-		std::cout << "Sides: a = " << a1 << ", b = " << b1 << ", c = " << c1 << ", d = " << d1 << std::endl;
-		std::cout << "Angles: A = " << A1 << ", B = " << B1 << ", C = " << C1 << ", D = " << D1 << std::endl;
-		std::cout << std::endl;
-	}
 };
 
 class Rectangle : public Quadrangle {
+protected:
+	std::string check() override {
+		return "Wrong";
+	}
 public:
 	Rectangle() : Quadrangle(10, 20, 10, 20, 90, 90, 90, 90, "Rectangle") {}
-	void print_info() override {
-		std::cout << Name << ":" << std::endl;
-		std::cout << check() << std::endl;
-		std::cout << "Amount of sides: " << amount() << std::endl;
-		std::cout << "Sides: a = " << a1 << ", b = " << b1 << ", c = " << c1 << ", d = " << d1 << std::endl;
-		std::cout << "Angles: A = " << A1 << ", B = " << B1 << ", C = " << C1 << ", D = " << D1 << std::endl;
-		std::cout << std::endl;
-	}
 };
 
 class Rhomb : public Quadrangle {
+protected:
+	std::string check() override {
+		return "Wrong";
+	}
 public:
 	Rhomb() : Quadrangle(30, 30, 30, 30, 30, 40, 30, 40, "Rhomb") {}
-	void print_info() override {
-		std::cout << Name << ":" << std::endl;
-		std::cout << check() << std::endl;
-		std::cout << "Amount of sides: " << amount() << std::endl;
-		std::cout << "Sides: a = " << a1 << ", b = " << b1 << ", c = " << c1 << ", d = " << d1 << std::endl;
-		std::cout << "Angles: A = " << A1 << ", B = " << B1 << ", C = " << C1 << ", D = " << D1 << std::endl;
-		std::cout << std::endl;
-	}
 };
 
 class Square : public Quadrangle {
@@ -163,14 +135,6 @@ protected:
 	}
 public:
 	Square() : Quadrangle(20, 20, 20, 20, 90, 90, 90, 90, "Square") {}
-	void print_info() override {
-		std::cout << Name << ":" << std::endl;
-		std::cout << check() << std::endl;
-		std::cout << "Amount of sides: " << amount() << std::endl;
-		std::cout << "Sides: a = " << a1 << ", b = " << b1 << ", c = " << c1 << ", d = " << d1 << std::endl;
-		std::cout << "Angles: A = " << A1 << ", B = " << B1 << ", C = " << C1 << ", D = " << D1 << std::endl;
-		std::cout << std::endl;
-	}
 };
 
 int main()
@@ -203,13 +167,4 @@ int main()
 	rhomb1->print_info();
 	Figure* square1 = &square;
 	square1->print_info();
-	/*triangle.print_info();
-	quadrangle.print_info();
-	rectangularTriangle.print_info();
-	isoscelesTriangle.print_info();
-	equalateralTriangle.print_info();
-	parallelogram.print_info();
-	rectangle.print_info();
-	rhomb.print_info();
-	square.print_info();*/
 }
